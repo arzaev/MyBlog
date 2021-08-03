a29b3ad516bd1b4a61967e7341a65bcb4f3ffcb9
arzaev <arzaevilya@gmail.com>
2021-08-04 06:19:07 +0700

    1v

--- blog/urls.py
+++ blog/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path(r'', views.ArticleListView.as_view(), name='main'),
-	path('<int:pk>/', views.ArticleDetailView.as_view())
+	path('<int:pk>/', views.ArticleDetailView.as_view()),
+	path('category/<int:category_id>', views.ArticleListCategoryView.as_view()),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
-from .models import Article
+from .models import Article, Category
 
 # Create your views here.
 
@@ -12,7 +12,25 @@ class ArticleListView(ListView):
 	model = Article
 	queryset = Article.objects.all()
 	template_name = "main.html"
-	paginate_by = 8
+	paginate_by = 6
+
+	def get_context_data(self, **kwargs):
+		context = super().get_context_data(**kwargs)
+		context['categories'] = Category.objects.all()
+		return context
+
+
+class ArticleListCategoryView(ListView):
+	template_name = "main.html"
+	paginate_by = 6
+
+	def get_queryset(self):
+		return Article.objects.filter(article_category=self.kwargs['category_id'])
+
+	def get_context_data(self, **kwargs):
+		context = super().get_context_data(**kwargs)
+		context['categories'] = Category.objects.all()
+		return context
 
 class ArticleDetailView(DetailView):
     model = Article
